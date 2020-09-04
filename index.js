33d6ed08de86bbfdd7138f2be023ca4e450d779a
royeradames <royeraadames@gmail.com>
2020-09-04 13:43:45 -0400

    stretch goal reatch

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ your file is getting way too big, bring a Router and make it thin, don't worry, 
 there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
 I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
 
-Go code!
+Go code! 
 */
 // imports
 const express = require("express")
